a8eed08d02770119d52a34129189ae0df666a92d
Bastien BUIL <bastien.buil@ecole.ensicaen.fr>
2023-01-11 09:44:56 +0100

    Ajout de la structure de l'application et du scanner de code bar

---
+++ ConsoScore/app/src/main/java/fr/consoscore/scanner/MainActivity.java
@@ -0,0 +1,53 @@
+package fr.consoscore.scanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+            intentIntegrator.setPrompt("Scanner le produit à analyser");
+            intentIntegrator.setOrientationLocked(false);
+            intentIntegrator.setBeepEnabled(true);
+            intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.EAN_13);
+            intentIntegrator.initiateScan();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent myIntent = new Intent(this, ResultActivity.class);
+                myIntent.putExtra("EAN", intentResult.getContents());
+                startActivity(myIntent);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+}
---
+++ ConsoScore/app/src/main/java/fr/consoscore/scanner/ResultActivity.java
@@ -0,0 +1,33 @@
+package fr.consoscore.scanner;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        String EAN = getIntent().getExtras().getString("EAN", "Error");
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return true;
+            }
+        });
+        webView.loadUrl("https://www.google.com/search?q=" + EAN);
+
+    }
+
+
+}
